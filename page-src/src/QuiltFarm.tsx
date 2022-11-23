f31d40fb97bb71b8e02ba039b9d2f027c48a9611
Nathan Miller <nathanmiller@fastmail.com>
2022-11-22 20:22:15 -0800

    asdf

--- page-src/src/QuiltFarm.tsx
+++ page-src/src/QuiltFarm.tsx
@@ -17,31 +17,28 @@ export function QuiltFarm(): React.ReactElement {
                 called <NewWindowLink href="https://www.mapeditor.org/">Tiled</NewWindowLink> re-created many of the blocks for use on my site.
             </BodyText>
 
-            <BodyText>
-                In addition to the website itself I also created a <NewWindowLink href="https://twitter.com/quilt_farm">Twitter bot</NewWindowLink> that tweets a random quilt each morning.
-            </BodyText>
+            <Section>Examples</Section>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+                <div style={{ marginRight: "1em" }}>
+                    <NewWindowLink href="https://quilt.farm/q/AQADAOY_Xl9lABAQAP8AAAD_AgA">
+                        <img src="/images/quilt-farm/AQADAOY_Xl9lABAQAP8AAAD_AgA.png" alt="quilt"></img>
+                    </NewWindowLink>
+                </div>
 
-            <Section>Example</Section>
-            <BodyText>
-                <NewWindowLink href="https://quilt.farm/q/AQADAOY_Xl9lABAQAP8AAAD_AgA">
-                    <img src="https://quiltfarm.azureedge.net/images/6zpzt4m0ETYznUnRZ8t7973mpTI2/c70fbd69-9b40-47ab-86d7-691205ca5578.png" alt="quilt"></img>
-                </NewWindowLink>
-
-                <NewWindowLink href="https://quilt.farm/q/AQADAHq4fiZVAAAAAP8AAAD_AgA">
-                    <img src="https://quiltfarm.azureedge.net/images/6zpzt4m0ETYznUnRZ8t7973mpTI2/29b88e5e-d993-4f19-9d74-a5d0d5353549.png" alt="quilt"></img>
-                </NewWindowLink>
+                <div style={{ marginRight: "1em" }}>
+                    <NewWindowLink href="https://quilt.farm/q/AQADAHq4fiZVAAAAAP8AAAD_AgA">
+                        <img src="/images/quilt-farm/AQADAHq4fiZVAAAAAP8AAAD_AgA.png" alt="quilt"></img>
+                    </NewWindowLink>
+                </div>
 
                 <NewWindowLink href="https://quilt.farm/q/AQACAFusKkFlAAAAAgIAAAD_AgA">
-                    <img src="https://quiltfarm.azureedge.net/images/6zpzt4m0ETYznUnRZ8t7973mpTI2/fffaa282-b1ce-417b-b29c-524506a93c0c.png" alt="quilt"></img>
+                    <img src="/images/quilt-farm/AQACAFusKkFlAAAAAgIAAAD_AgA.png" alt="quilt"></img>
                 </NewWindowLink>
-            </BodyText>
+            </div>
 
             <Section>Technology</Section>
             <BodyText>
                 Built using TypeScript, React, C# and ASP .NET.
-            </BodyText>
-            <BodyText>
-                On the backend the source quilt blocks are stored in a single file in Azure Blob Storage. Saved quilt images are stored in Blob Storage and served via a CDN.
             </BodyText>
         </>
     );
